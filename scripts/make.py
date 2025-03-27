0c1ff2251b268c47445e244f514c73b114c6de40
Daniel Novomeský <dnovomesky@gmail.com>
2025-03-27 16:01:51 +0100

    Experimental Qt5 build

--- scripts/make.py
+++ scripts/make.py
@@ -27,7 +27,7 @@ class NomacsConfig(Config):
 
         # tune cmake parameters here
         args = [
-            "-DQT_VERSION_MAJOR=6",
+            "-DQT_VERSION_MAJOR=5",
             "-DCMAKE_PREFIX_PATH=" + self.qtpath,
             "-DDEPENDENCY_PATH=" + self.libpath,
             "-DENABLE_QUAZIP=ON",
@@ -160,7 +160,7 @@ class QuazipConfig(Config):
 
         super().defaults()
 
-        self.binaryfile = os.path.join(self.builddir, "quazip", "Release", "quazip1-qt6.dll")
+        self.binaryfile = os.path.join(self.builddir, "quazip", "Release", "quazip1-qt5.dll")
 
     def cmake_args(self):
 
@@ -170,7 +170,7 @@ class QuazipConfig(Config):
             "-B" + self.builddir,
             "-DQUAZIP_BZIP2=OFF",
             "-DQUAZIP_BZIP2_STDIO=OFF",
-            "-DQUAZIP_QT_MAJOR_VERSION=6",
+            "-DQUAZIP_QT_MAJOR_VERSION=5",
             self.srcpath
         ]
 
@@ -266,14 +266,14 @@ class FormatsConfig(Config):
 
         # tune cmake parameters here
         args = [
-            "-DQT_VERSION_MAJOR=6",
+            "-DQT_VERSION_MAJOR=5",
             "-DCMAKE_PREFIX_PATH=" +
             # os.path.join(self.libpath, "libde265") + ";" +
             # os.path.join(self.libpath, "libheif") + ";" +
             self.qtpath,
             "-DENABLE_QOI=ON",
-            "-DENABLE_WEBP=OFF",
-            "-DENABLE_DDS=ON",
+            "-DENABLE_WEBP=ON",
+            "-DENABLE_DDS=OFF",
             "-B" + self.builddir,
             self.srcpath
         ]
